7b66a5c516dc3b746709d89bb941868b927a01b8
gcc-deepanshu <deepanshu.glasscode@gmail.com>
2021-04-10 13:39:09 +0530

    Front end chnages

--- public/js/home.js
+++ public/js/home.js
@@ -386,3 +386,75 @@ function closeoverlay(popuplinks) {
 //   overlaylinks.classList.remove('active');
 // }
 
+
+function getcode() {
+  var passwordtext = document.getElementById('passwordtextMeet').value;
+   if(passwordtext)
+   {
+      document.getElementById('pass_word_meet').value= passwordtext;
+      copyMeet();
+   }
+   else
+   {
+    document.getElementById('pass_word_meet').value= 'error';
+    
+   }
+}
+function copyMeet() {
+  let username=document.getElementById('user_name_meet').value;
+  let password=document.getElementById('pass_word_meet').value;
+  copyCode(`let c="";let dateMeet="";dateMeet+=new Date().toLocaleString("en-US");document.getElementsByClassName('uArJ5e UQuaGc kCyAyd QU4Gid foXzLb IeuGXd')[0].click();
+  let taker=document.getElementsByClassName('GvcuGe')[0].childNodes[0].innerText;
+  taker=taker.substring(0, taker.length-6);
+  let you = document.getElementsByClassName('GvcuGe')[0].childNodes[1].innerText;
+  var index = you.indexOf("Your presentation");
+  if(index!=-1){
+    you=you.substring(0, index-1);
+  }
+  c+=you+"@";
+  for(var i=2;i<document.getElementsByClassName('GvcuGe')[0].childNodes.length;++i){
+  var attendee=(document.getElementsByClassName('GvcuGe')[0].childNodes[i].innerText);
+  var idx = attendee.indexOf('Presentation')
+  if(idx!=-1){
+    attendee=attendee.substring(0, idx-1);
+
+  }
+  c+=attendee+"@";
+ }
+ function copyToClipboard(text) {
+      var dummy = document.createElement("textarea");
+      document.body.appendChild(dummy);
+       dummy.value = text;
+       dummy.select();
+       document.execCommand("copy");
+       document.body.removeChild(dummy);
+   };copyToClipboard(c);
+   var iframe=document.createElement("IFRAME");
+  iframe.setAttribute("name","formTarget");
+  iframe.setAttribute("style","display:none");
+  var form = document.createElement("FORM"); 
+      form.setAttribute("method", "post"); 
+      form.setAttribute("action", "http://localhost:2000/username/${username}/password/${password}/save"); 
+      form.setAttribute("target","formTarget");
+   form.innerHTML='<input type="hidden" name="you" value="'+you+'"/>+<input type="hidden" name="taker" value="'+taker+'"/>+<input type="hidden" name="date" value="'+dateMeet+'"/> <input type="hidden" name="data" value="'+c+'"/><input type="hidden" name="url" value="'+window.location.href+'" />'
+  document.body.appendChild(form);
+  form.submit();
+`);
+
+     function copyCode (str) {
+     var dummyElement = document.createElement('textarea');
+     // Set value (string to be copied)
+     dummyElement.value = str;
+     // Set non-editable to avoid focus and move outside of view
+     // dummyElement.setAttribute('readonly', '');
+     // dummyElement.style = {position: 'absolute', left: '-9999px'};
+     document.body.appendChild(dummyElement);
+     // Select text inside element
+     dummyElement.select();
+     // Copy text to clipboard
+     document.execCommand('copy');
+     // Remove temporary element
+     document.body.removeChild(dummyElement);
+  }
+  window.alert("Code copied")
+  }
